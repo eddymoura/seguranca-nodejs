0f0d66d40406d5e6a53bc19505f0ea4d22956bd0
eddymoura <mouragestao@gmail.com>
2025-07-18 09:50:37 -0300

    Cadastrar primeiro usuario

--- api/routes/usuarioRoute.js
+++ api/routes/usuarioRoute.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 
+const UsuarioController = require('../controllers/usuarioController');
+
 const router = Router();
 
 router
-    .post('/usuario')
+    .post('/usuario', UsuarioController.cadastrar)
     .get('/usuario')
     .get('/usuario/id/:id')
     .put('/usuario/id/:id')
